313857b2820f6478435ec047015690f13b90b294
oficinainfo <167123760+oficinainfo@users.noreply.github.com>
2024-04-16 12:44:25 -0300

    Create analytics.js

---
+++ heyleia.com/js/analytics.js
@@ -0,0 +1,18 @@
+/*$(document).ready(function() {
+    setTimeout(function() {
+        if (window.parent === window.self) {
+            setInterval(function() {
+                $.ajax({
+                    url: '/updateDuration.php',
+                    type: 'POST',
+                    success: function(data) {
+                        console.log(data);
+                    },
+                    error: function(err) {
+                        console.log(err);
+                    }
+                });
+            }, 60000);
+        }
+    }, 60000);
+});*/
